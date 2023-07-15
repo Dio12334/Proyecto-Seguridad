f37d65e0a06b7e7114ce169c2d182275c66598dd
Alexandra-SR <alexandra.shulca@utec.edu.pe>
2023-07-15 10:19:35 -0500

    update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import os
 load_dotenv()
 
 
- 
+
 app = Flask(__name__)
 
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
--- src/password_generator.py
+++ src/password_generator.py
@@ -2,7 +2,7 @@ import random
 import string
 
 def generate_password(length=16):
-    # """Generate a random password."""
+    # Generate a random password
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
